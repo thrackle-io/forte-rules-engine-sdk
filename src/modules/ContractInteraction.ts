fd966ac1fd1f230d5c4b85e6fc086fb78a7bc689
Michael Peterson <michaelpeterson@michaels-mbp.myfiosgateway.com>
2024-12-16 09:54:45 -0500

    Corrected abi issues

--- src/modules/ContractInteraction.ts
+++ src/modules/ContractInteraction.ts
@@ -15,7 +15,7 @@ import { parseSyntax, TrackerDefinition, buildForeignCallList, buildForeignCallL
 import { privateKeyToAccount } from 'viem/accounts';
 
 import RulesEngineRunLogicArtifact from "../abis/RulesEngineDataFacet.json";
-import RulesDiamondArtifact from "../abis/RulesEngineMainFacet.json";
+import RulesDiamondArtifact from "../abis/RulesEngineDiamond.json";
 
 const account = privateKeyToAccount(
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // TODO: This is a foundry private key, replace with being read from .env/config
