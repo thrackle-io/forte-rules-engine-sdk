66b30fb3841d6a0a37cb9a101304f42b2d1e010e
RJ Catalano <richard@thrackle.io>
2025-01-10 13:55:55 -0600

    new wagmi config
    Signed-off-by: RJ Catalano <richard@thrackle.io>

---
+++ config.ts
@@ -0,0 +1,22 @@
+import { createConfig } from '@wagmi/core'
+import { privateKeyToAccount } from 'viem/accounts'
+import { foundry } from '@wagmi/core/chains'
+import { http, walletActions, publicActions, createTestClient, PrivateKeyAccount } from 'viem'
+
+const foundryPrivateKey: `0x${string}` = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+export const account: PrivateKeyAccount = privateKeyToAccount(foundryPrivateKey)
+
+export const DiamondAddress: `0x${string}` = `0x5FC8d32690cc91D4c39d9d3abcBD16989F875707`
+
+export const config = createConfig({
+    chains: [foundry],
+    client({ chain }) { 
+      return createTestClient(
+        { chain,
+          transport: http('http://localhost:8545'),
+          mode: 'anvil'
+        }
+      ).extend(walletActions).extend(publicActions)
+    }, 
+  })
+
