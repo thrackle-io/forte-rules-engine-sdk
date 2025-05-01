47a13432250ca810783e6f8869d02bc3a12ea643
Michael Peterson <michaelpeterson@michaels-mbp.myfiosgateway.com>
2025-05-01 12:59:09 -0400

    Removed export from index that no longer exists

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 /// SPDX-License-Identifier: BUSL-1.1
-// index.ts
-
-export {
-    initializeRulesEngineConnection,
-    createPolicy,
-    getPolicy,
-    updatePolicy,
-    deletePolicy,
-    applyPolicy,
-    createRule,
-    getRule,
-    getAllRules,
-    updateRule,
-    deleteRule,
-    createFunctionSignature,
-    deleteFunctionSignature,
-    createTracker,
-    getTracker,
-    getAllTrackers,
-    updateTracker,
-    deleteTracker,
-    createForeignCall,
-    getForeignCall,
-    getAllForeignCalls,
-    updateForeignCall,
-    deleteForeignCall,
-} from './src/modules/RulesEngineInteractions';
+// index.js
 
 export {
     RulesEngine
