9cc21b5151a77c42ba2038ce9526feaf6a2b309a
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2025-05-05 13:35:55 -0500

    Update UserContract.sol back to pristine state

--- tests/testOutput/UserContract.sol
+++ tests/testOutput/UserContract.sol
@@ -1,25 +1,23 @@
 /// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
-import "tests/testOutput/testFileA.sol";
-import "src/testOutput/testFileA.sol";
 
 /**
- * @title UserContract Contract for Testing the Rules Engine
+ * @title Example Contract for Testing the Rules Engine
  * @author @mpetersoCode55, @ShaneDuncan602, @TJ-Everett, @VoR0220
  * @dev This file contains an example Solidity contract designed to demonstrate and test the integration of the Rules Engine.
- *              It provides a sample function that showcases how custom arguments can be sent to the Rules Engine.
- * 
+ *              It provides a sample function that can be used to showcase how custom arguments are sent to the Rules Engine.
  */
-contract ExampleUserContract is RulesEngineClientCustom {
+contract ExampleUserContract {
+  
     /**
      * @notice Transfers a specified amount of tokens to a given address.
-     * @dev This function allows transferring tokens to another address with an additional parameter.
+     * @dev This function allows transferring tokens with an additional parameter.
      * @param to The address of the recipient.
      * @param value The amount of tokens to transfer.
-     * @param somethingElse An additional parameter for custom logic (purpose not specified in the given code).
+     * @param somethingElse An additional parameter for custom logic.
      * @return bool Returns true if the transfer is successful.
      */
-    function transfer(address to, uint256 value, uint256 somethingElse) public checkRulesBeforetransfer(to, value, somethinElse) returns (bool) {
+    function transfer(address to, uint256 value, uint256 somethingElse) public returns (bool) {
         somethingElse;
         to;
         value;
