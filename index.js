3d287777d656900f0e7cb3299cf84e7ad2a194c6
Danny Mulvihill <danny.mulvihill@thrackle.io>
2025-05-12 09:59:56 -0500

    change name back to index.js, remove include directive as no longer needed

---
+++ index.js
@@ -0,0 +1,40 @@
+/// SPDX-License-Identifier: BUSL-1.1
+// index.js
+
+export { RulesEngine } from "./src/modules/rules-engine.ts";
+
+export { getConfig, connectConfig, setupConfig } from "./config.ts";
+
+export {
+  RulesEnginePolicyContract,
+  RulesEngineComponentContract,
+  FCNameToID,
+  RuleStorageSet,
+  hexToFunctionSignature,
+  PolicyJSON,
+  foreignCallJSON,
+  ruleJSON,
+  EffectType,
+  RuleStruct,
+  ForeignCallDefinition,
+  PlaceholderStruct,
+  IndividualArugmentMapping,
+  ForeignCallArgumentMappings,
+  FunctionArgument,
+  stringReplacement,
+  trackerIndexNameMapping,
+  TrackerDefinition,
+  RawData,
+  matchArray,
+  truMatchArray,
+  operandArray,
+  supportedTrackerTypes,
+  pTypeEnum,
+  PT,
+} from "./src/modules/types.ts";
+
+export { generateModifier } from "./src/codeGeneration/generate-solidity.ts";
+
+export { injectModifier } from "./src/codeGeneration/inject-modifier.ts";
+
+export { policyModifierGeneration } from "./src/codeGeneration/code-modification-script.ts";
